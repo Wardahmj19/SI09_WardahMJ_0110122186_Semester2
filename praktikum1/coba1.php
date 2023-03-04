a57c60fa801c87025d7a6729c9da96fadeceabf5
Wardahmj19 <wardahmuzayyinatuljannah@gmail.com>
2023-03-05 00:08:21 +0700

    Praktikum_1_PHP

---
+++ praktikum1/coba1.php
@@ -0,0 +1,8 @@
+<?php
+
+    define('PHI', 3.14);
+    define('DBNAME', 'inventori');
+    define('DBSERVER', 'localhost');
+
+    echo 'Nama Database nya: ' .DBNAME;
+    echo '<br> Lokasi Database nya: ' .DBSERVER;
