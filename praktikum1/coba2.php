25af22067d3ab217e28ab4ef31b7dd49af9dbf76
Wardahmj19 <wardahmuzayyinatuljannah@gmail.com>
2023-05-16 20:38:13 +0700

    Praktikum_1

---
+++ praktikum1/coba2.php
@@ -0,0 +1,8 @@
+<?php
+
+    $tims = ["Erwin", "Heru", "Ali", "Jaki"];
+
+    foreach($tims as $person){
+
+        echo $person. '<br>';
+    }
---
+++ praktikum1/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    $nama = 'Wardah';
+    $umur = 20;
+    $berat = 20;
+
+    echo 'Nama Saya: ' . $nama;
+    echo '<br> Umur Saya: ' . $umur;
+    echo '<br> Berat Saya: ' . $berat;
